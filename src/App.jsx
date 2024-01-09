f1ba0659ebb098ba57ffdf39fe9e47e790cfa97e
DatTranTien <trangauhn@gmail.com>
2024-01-09 14:43:45 +0700

    Dark mode & Light mode use Css Variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { Typography } from '@mui/material'
 import Button from '@mui/material/Button'
+import {
+  useColorScheme
+} from '@mui/material/styles'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
       <h3>DatTranTien</h3>
+      <ModeToggle/>
+      <Typography variant="h1" component="h2" color="text.secondary">
+  h1. Heading
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
