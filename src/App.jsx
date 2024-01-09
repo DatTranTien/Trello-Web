99ec1fba5e78deac20587e44d288e8d7a79a5e84
DatTranTien <trangauhn@gmail.com>
2024-01-09 12:02:49 +0700

    setup CssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
-import Icon from '@mui/material/Icon'
+import Button from '@mui/material/Button'
+
 
 function App() {
   return (
     <>
-      <Icon>star</Icon>;
+      <h3>DatTranTien</h3>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
   )
 }
