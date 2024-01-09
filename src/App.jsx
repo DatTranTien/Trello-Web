8175d120e8820c3b529890d76dc64211c4d01a26
DatTranTien <trangauhn@gmail.com>
2024-01-09 16:06:21 +0700

    set theme base on theme system

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import { Typography } from '@mui/material'
+import { FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
 import {
   useColorScheme
 } from '@mui/material/styles'
 
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m:1, minWidth:20 }} size='small'>
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>Light</MenuItem>
+        <MenuItem value='dark'>Dark</MenuItem>
+        <MenuItem value='system'>System</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
   return (
@@ -21,6 +45,7 @@ function App() {
   return (
     <>
       <h3>DatTranTien</h3>
+      <ModeSelect/>
       <ModeToggle/>
       <Typography variant="h1" component="h2" color="text.secondary">
   h1. Heading
